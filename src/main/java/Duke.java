42fc5064dc7b6ff344d3a82359b8334f2df68a4e
Lee Jian Ann <leejianann95@gmail.com>
2023-09-10 18:51:19 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Jabot\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("Hello! I'm Jabot" + System.lineSeparator() + "What can I do for you?");
+
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        while(true){
+            if(line.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else{
+                System.out.println(line + System.lineSeparator());
+                line = in.nextLine();
+            }
+        }
     }
 }
